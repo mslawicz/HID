c0167918493c45665daba6fe9066922e1bf878e2
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-12-31 18:24:24 +0100

    keyboard report descriptor added; not tested

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -91,6 +91,31 @@
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
   /* USER CODE BEGIN 0 */
+	/* keyboard */
+	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+	0x09, 0x06,                    // USAGE (Keyboard)
+	0xa1, 0x01,                    // COLLECTION (Application)
+		0x85, 0x01,                    //   REPORT_ID (1)
+		0x05, 0x07,                    //   USAGE_PAGE (Key Codes)
+		0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+		0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+		0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+		0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+		0x75, 0x01,                    //   REPORT_SIZE (1)
+		0x95, 0x08,                    //   REPORT_COUNT (8)
+		0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+		0x95, 0x01,                    //   REPORT_COUNT (1)
+		0x75, 0x08,                    //   REPORT_SIZE (8)
+		0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
+		0x95, 0x06,                    //   REPORT_COUNT (6)
+		0x75, 0x08,                    //   REPORT_SIZE (8)
+		0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+		0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+		0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+		0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+		0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+	0xc0,                          // END_COLLECTION
+	/* mouse */
 	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
 	0x09, 0x02,                    // USAGE (Mouse)
 	0xa1, 0x01,                    // COLLECTION (Application)
--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -65,7 +65,7 @@
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22315
+#define USBD_PID_FS     22316
 #define USBD_PRODUCT_STRING_FS     "Human Interface Device"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
