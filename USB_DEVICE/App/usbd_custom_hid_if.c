b275b2772dd70789c1c9b71c81e6008c0a9f76d3
Marcin Sławicz <marcin.slawicz@gmail.com>
2023-01-01 11:23:52 +0100

    joystick with 8 axis, 32 buttons and HAT switch added to report descriptor

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -144,53 +144,54 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 			0x81, 0x06,                    //     INPUT (Data,Var,Rel)
 		0xc0,                              //   END_COLLECTION
 	0xC0,					// End Collection
-	/* gamepad 1 */
-	0x05, 0x01,				// Usage Page (Generic Desktop Ctrls)
-	0x09, 0x05,				// Usage (Game Pad)
-	0xA1, 0x01,				// Collection (Application)
-		0x85, 0x03,				//   Report ID (3)
-		0x05, 0x09,				//   Usage Page (Button)
-		0x19, 0x01,				//   Usage Minimum (Button 1)
-		0x29, 0x10,				//   Usage Maximum (Button 16)
-		0x15, 0x00,				//   Logical Minimum (0)
-		0x25, 0x01,				//   Logical Maximum (1)
-		0x75, 0x01,				//   Report Size (1)
-		0x95, 0x10,				//   Report Count (16)
-		0x81, 0x02,				//   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-		0x05, 0x01,				//   Usage Page (Generic Desktop Ctrls)
-		0x15, 0x81,				//   Logical Minimum (-127)
-		0x25, 0x7F,				//   Logical Maximum (127)
-		0x09, 0x30,				//   Usage (X)
-		0x09, 0x31,				//   Usage (Y)
-		0x09, 0x32,				//   Usage (Z)
-		0x09, 0x35,				//   Usage (Rz)
-		0x75, 0x08,				//   Report Size (8)
-		0x95, 0x04,				//   Report Count (4)
-		0x81, 0x02,				//   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,    /*     END_COLLECTION	             */
-	/* gamepad 2 */
-	0x05, 0x01,				// Usage Page (Generic Desktop Ctrls)
-	0x09, 0x05,				// Usage (Game Pad)
-	0xA1, 0x01,				// Collection (Application)
-		0x85, 0x04,				//   Report ID (4)
-		0x05, 0x09,				//   Usage Page (Button)
-		0x19, 0x01,				//   Usage Minimum (Button 1)
-		0x29, 0x10,				//   Usage Maximum (Button 16)
-		0x15, 0x00,				//   Logical Minimum (0)
-		0x25, 0x01,				//   Logical Maximum (1)
-		0x75, 0x01,				//   Report Size (1)
-		0x95, 0x10,				//   Report Count (16)
-		0x81, 0x02,				//   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-		0x05, 0x01,				//   Usage Page (Generic Desktop Ctrls)
-		0x15, 0x81,				//   Logical Minimum (-127)
-		0x25, 0x7F,				//   Logical Maximum (127)
-		0x09, 0x30,				//   Usage (X)
-		0x09, 0x31,				//   Usage (Y)
-		0x09, 0x32,				//   Usage (Z)
-		0x09, 0x35,				//   Usage (Rz)
-		0x75, 0x08,				//   Report Size (8)
-		0x95, 0x04,				//   Report Count (4)
-		0x81, 0x02,				//   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    /* joystick report */
+    0x05, 0x01,                    /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x04,                    /* USAGE (Joystick) */
+	0xa1, 0x01,                    /* COLLECTION (Application) */
+		0x85, 0x03,				   //   Report ID (3)
+		0x05, 0x01,                    /*   USAGE_PAGE (Generic Desktop) */
+		0x09, 0x01,                    /*   USAGE (Pointer) */
+		0xa1, 0x00,                    /*   COLLECTION (Physical) */
+			0x75, 0x10,                    /*     REPORT_SIZE (16) */
+			0x16, 0x01, 0x80,              /*     LOGICAL_MINIMUM (-32767) */
+			0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+			0x09, 0x30,                    /*     USAGE (X) */
+			0x09, 0x31,                    /*     USAGE (Y) */
+			0x09, 0x32,                    /*     USAGE (Z) */
+			0x09, 0x35,                    /*     USAGE (Rz) */
+			0x95, 0x04,                    /*     REPORT_COUNT (4) */
+			0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+			0x15, 0x00,                    /*     LOGICAL_MINIMUM (0) */
+			0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+			0x09, 0x33,                    /*     USAGE (Rx) */
+			0x09, 0x34,                    /*     USAGE (Ry) */
+			0x09, 0x36,                    /*     USAGE (slider) */
+			0x09, 0x37,                    /*     USAGE (dial) */
+			0x95, 0x04,                    /*     REPORT_COUNT (4) */
+			0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+		0xc0,                          /*   END_COLLECTION */
+		0x09, 0x39,                    /*   USAGE (Hat switch) */
+		0x15, 0x01,                    /*   LOGICAL_MINIMUM (1) */
+		0x25, 0x08,                    /*   LOGICAL_MAXIMUM (8) */
+		0x35, 0x00,                    /*   PHYSICAL_MINIMUM (0) */
+		0x46, 0x3b, 0x01,              /*   PHYSICAL_MAXIMUM (315) */
+		0x65, 0x14,                    /*   UNIT (Eng Rot:Angular Pos) */
+		0x75, 0x04,                    /*   REPORT_SIZE (4) */
+		0x95, 0x01,                    /*   REPORT_COUNT (1) */
+		0x81, 0x42,                    /*   INPUT (Data,Var,Abs, Null) */
+		0x75, 0x04,                    /*   REPORT_SIZE (4) */
+		0x95, 0x01,                    /*   REPORT_COUNT (1) */
+		0x81, 0x41,                    /*   INPUT (Cnst,Ary,Abs,Null) */
+		0x05, 0x09,                    /*   USAGE_PAGE (Button) */
+		0x19, 0x01,                    /*   USAGE_MINIMUM (Button 1) */
+		0x29, 0x20,                    /*   USAGE_MAXIMUM (Button 32) */
+		0x15, 0x00,                    /*   LOGICAL_MINIMUM (0) */
+		0x25, 0x01,                    /*   LOGICAL_MAXIMUM (1) */
+		0x75, 0x01,                    /*   REPORT_SIZE (1) */
+		0x95, 0x20,                    /*   REPORT_COUNT (32) */
+		0x55, 0x00,                    /*   UNIT_EXPONENT (0) */
+		0x65, 0x00,                    /*   UNIT (None) */
+		0x81, 0x02,                    /*   INPUT (Data,Var,Abs) */
   /* USER CODE END 0 */
   0xC0    /*     END_COLLECTION	             */
 };
