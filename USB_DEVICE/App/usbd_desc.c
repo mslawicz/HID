7328f279ed9eaa2224721a2f85042dcb312688fc
Marcin Sławicz <marcin.slawicz@gmail.com>
2023-01-01 10:30:37 +0100

    PID and name changed

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -62,11 +62,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0x483
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22316
-#define USBD_PRODUCT_STRING_FS     "Human Interface Device"
+#define USBD_PID_FS     0x5715
+#define USBD_PRODUCT_STRING_FS     "MultiHID"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
 
